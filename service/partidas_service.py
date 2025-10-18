2ee80c1c432c2845558cbafab1213160711289ec
Gabriel <gabrielrangelsilva250@gmail.com>
2025-10-18 14:30:10 -0300

    Modificação 1.0.0

---
+++ service/partidas_service.py
@@ -0,0 +1,13 @@
+class PartidaService:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def criar_partida(self, partida_data: PartidaCreate) -> PartidaModels:
+        nova_partida = PartidaModels(**partida_data.dict())
+        self.db.add(nova_partida)
+        self.db.commit()
+        self.db.refresh(nova_partida)
+        return nova_partida
+
+    def buscar_partida(self, partida_id: int) -> Optional[PartidaModels]:
+        return self.db.query(PartidaModels).filter_by(id=partida_id).first()
