db1abc7bb9031b9f4ac6fc77dd3c0d333769f695
Gabriel <gabrielrangelsilva250@gmail.com>
2025-10-18 19:13:15 -0300

    Fix typos in database.py and update settings.json for cSpell; correct AsyncSession references and improve session management

--- core/database.py
+++ core/database.py
@@ -1,19 +1,19 @@
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine, AsyncSessiom
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine, AsyncSession
 from core.configs import settings
 from contextlib import asynccontextmanager
 
-engine: AsyncEngine = create_async_engine(settings.DB_URL)
+engine: AsyncEngine = create_async_engine(settings.DB_URL, echo=True)
 
 Session = sessionmaker(
     autocommit=False,
-    autoflush = False,
-    expire_on_commit = False,
-    class_=AsyncSessiom,
+    autoflush=False,
+    expire_on_commit=False,
+    class_=AsyncSession,
     bind=engine
 )
 
 @asynccontextmanager
-async def get_session() -> AsyncSessiom:
-    async whit Session() as session:
-    yield session
+async def get_session() -> AsyncSession:
+    async with Session() as session:
+        yield session
