8845a5d1269c33ee48ef844944e4cf30cf9933e7
Gabriel <gabrielrangelsilva250@gmail.com>
2025-10-18 18:02:41 -0300

    Refactor main.py to remove unused APIRouter and streamline router inclusion

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI
 from core.configs import settings
+
+# Importando os routers das APIs
 from api.endpoints.jogador_routes import jogador_router
 from api.endpoints.mensagem_routes import mensagem_router
 from api.endpoints.navio_routes import navio_router
 from api.endpoints.partida_routes import partida_router
 from api.endpoints.turno_routes import turno_router
 
-app = FastAPI (title="MarineAPI 1.0.0")
+app = FastAPI(title="MarineAPI 1.0.0")
 
+# Incluindo os routers na aplicação
+app.include_router(jogador_router)
+app.include_router(mensagem_router)
+app.include_router(navio_router)
+app.include_router(partida_router)
+app.include_router(turno_router)
 
-api_router = APIRouter()
-
-app.include_router(api_router)
-
-if __name__ == "__main__"
-import uvicorn
-uvicorn.run("maim:app",host="127.0.0.1",port=8000, log_level="info", reload=True)
+# Execução local
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info", reload=True)
