3af5dfa74bfe19b7a6c52467b3d4193b41fed2a7
Gabriel <gabrielrangelsilva250@gmail.com>
2025-10-19 10:14:34 -0300

    Refactor jogador service: update methods to use async/await for improved performance and consistency

--- services/jogador_service.py
+++ services/jogador_service.py
@@ -1,18 +1,22 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from models.jogador_models import JogadorModels
 from schemas.jogador_schemas import Jogador
 from typing import Optional
+from sqlalchemy import select
 
 class JogadorService:
-    def __init__(self, db: Session):
+    def __init__(self, db: AsyncSession):
         self.db = db
 
-    def criar_jogador(self, dados: Jogador) -> JogadorModels:
+    async def criar_jogador(self, dados: Jogador) -> JogadorModels:
         novo_jogador = JogadorModels(**dados.model_dump())
         self.db.add(novo_jogador)
-        self.db.commit()
-        self.db.refresh(novo_jogador)
+        await self.db.commit()
+        await self.db.refresh(novo_jogador)
         return novo_jogador
 
-    def buscar_por_id(self, jogador_id: int) -> Optional[JogadorModels]:
-        return self.db.query(JogadorModels).filter_by(id=jogador_id).first()
+    async def buscar_por_id(self, jogador_id: int) -> Optional[JogadorModels]:
+        result = await self.db.execute(
+            select(JogadorModels).filter_by(id=jogador_id)
+        )
+        return result.scalar_one_or_none()
